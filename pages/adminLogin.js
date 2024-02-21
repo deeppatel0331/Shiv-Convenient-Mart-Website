0aca331b390ad1e69fab4327ffe5098d217b592e
deeppatel0331 <dap5903@psu.edu>
2024-02-21 15:02:44 -0500

    Improved appearance

--- pages/adminLogin.js
+++ pages/adminLogin.js
@@ -24,18 +24,50 @@ const AdminLogin = () => {
 
   return (
     <div>
-        adminLogin
-        <input 
-        type='email'
-        ref={emailRef}
-        />
-        <input 
-        type='password'
-        ref={passRef}
-        />
-        <button onClick={loginFunction}>Login</button>
+        <ParentContainer>
+            <HeadContainer>Admin Login</HeadContainer>
+            
+            <Info>Email:</Info>
+            <input 
+            type='email'
+            ref={emailRef}
+            />
+
+            <Info>Passoword:</Info>
+            <input 
+            type='password'
+            ref={passRef}
+            />
+
+            <Info></Info>
+            <button onClick={loginFunction}>Login</button>
+        </ParentContainer>
     </div>
   )
 }
 
+const ParentContainer = styled.main`
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #141e30;
+`
+const HeadContainer = styled.h1`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 50px;
+    font-family: Sans Seriff;
+    color: white;
+`
+
+const Info = styled.p`
+    margin: 5px;
+    font-size: 20px;
+    font-family: Sans Seriff;
+    color: white;
+`;
 export default AdminLogin
