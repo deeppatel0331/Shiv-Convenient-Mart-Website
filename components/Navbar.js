58c22a958b4e3044ef20eecd3cb65fa10e8a9c9a
deeppatel0331 <dap5903@psu.edu>
2024-04-03 15:23:22 -0400

    Good to go

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,9 +7,9 @@ import { useRouter } from "next/router";
 * It is used to display the name of the website and provide buttons to navigate to different parts of the website.
 */
 
-//gives the buttons the functionality to go to the corresponding page when clicked
 export const Navbar = () => {
 
+    //gives the buttons the functionality to go to the corresponding page when clicked
     const router = useRouter();
 
     function goToHome(){
@@ -20,13 +20,14 @@ export const Navbar = () => {
         router.push("/information")
     }
 
-    function goToLoginPage(){
-        router.push("/login")
-    }
-
     function goToCreateNFTPage(){
         router.push("/createnft")
     }
+
+    function goToConnectPage(){
+        router.push("/connectwallet")
+    }
+
 
     //layout of the navigation bar
     return (
@@ -41,10 +42,6 @@ export const Navbar = () => {
             <NavigationElement onClick={goToHome}>
                 Home
             </NavigationElement>
-
-            <NavigationElement onClick={goToLoginPage}>
-                Log-in
-            </NavigationElement>
             
             <NavigationElement onClick={goToInformationPage}>
                 Information
@@ -54,13 +51,17 @@ export const Navbar = () => {
                 Create NFT
             </NavigationElement>
 
+            <NavigationElement onClick={goToConnectPage}>
+                Connect Wallet
+            </NavigationElement>
+
         </NavigationButtonHolder>
 
     </Container>
     )
 }
 
-//holds the navigation bar background-image: linear-gradient(to bottom, #141e30, #243b55);
+//holds the navigation bar background-image:
 const Container = styled.div`
     width: 100vw;
     display: flex;
@@ -73,9 +74,9 @@ const Container = styled.div`
 `
 //essentially the title of the logo box/website
 const LogoBox = styled.div`
-    font-size: 5vw;
+    font-size: 5.5vw;
     font-weight: bold;
-    font-family: Sans Seriff;
+    font-family: sans seriff;
     background: #00BFFF;
     -webkit-background-clip: text;
     color: transparent;
@@ -86,22 +87,23 @@ const NavigationButtonHolder = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 25vw;
+    width: 30vw;
     gap: 1vw
 `
 
 //the buttons themselves
 const NavigationElement = styled.button`
-    padding: 0.75vw;
+    padding: 0.80vw;
     background: #00BFFF;
     color: black;
+    font-family: 'Roboto', sans-serif;
     border-radius: 0.5vw;
     border: none;
     cursor: pointer;
 
     transition: 0.3s ease-in-out;
     &:hover{
-        transform: scale(1.25);
+        transform: scale(1.15);
         background: red;
         color: black;
     }
